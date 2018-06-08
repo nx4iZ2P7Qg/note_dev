602a859707fa005c48fddc13008baf1fad0975ec
DF <fzcfydcz@163.com>
2018-06-08 17:35:53 +0800

    累积

---
+++ Linux/smartctl.sh
@@ -0,0 +1,22 @@
+# 检查硬盘是否支持smart
+smartctl -i /dev/sda
+
+# 检查硬盘健康状态
+smartctl -H /dev/sda
+
+# 查看全部信息
+smartctl -A /dev/sda
+
+# 检测硬盘
+smartctl -t short /dev/sda # 后台检测硬盘，消耗时间短
+smartctl -t long /dev/sda # 后台检测硬盘，消耗时间长
+smartctl -C -t short /dev/sda # 前台检测硬盘，消耗时间短
+smartctl -C -t long /dev/sda # 前台检测硬盘，消耗时间长
+# 中断检测
+smartctl -X /dev/sda
+
+# 查看检测进度用结果，4T硬盘差不多需要10小时
+smartctl -l selftest /dev/sda
+
+# 查看错误日志
+smartctl -l error /dev/sda
