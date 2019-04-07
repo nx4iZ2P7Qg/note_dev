94283410f090a29da57d47ac4165c55fa5432b7c
fzcfydcz <fzcfydcz@163.com>
2019-04-07 12:50:45 +0800

    更新 docker_demo.sh

--- Linux/docker_demo.sh
+++ Linux/docker_demo.sh
@@ -93,3 +93,40 @@ docker tag container_id df/centos:newtag
 
 # 导出当前运行的容器
 docker export -o certusvmanagerdriver.tar e07593dc83d5
+
+# 登录，才能往docker上推自己的镜像
+docker login
+
+
+############################ svn ############################
+# Dockerfile
+FROM alpine:3.8
+
+CMD [ "/usr/bin/svnserve", "--daemon", "--foreground", "--root", "/var/opt/svn" ]
+EXPOSE 3690
+VOLUME [ "/var/opt/svn" ]
+WORKDIR /var/opt/svn
+
+RUN apk add --no-cache subversion==1.10.4-r0
+
+# 参考 https://hub.docker.com/r/garethflowers/svn-server
+
+# 启动容器
+docker run \
+    --name svn-server \
+    --detach \
+    --volume /home/svn:/var/opt/svn \
+    --publish 3690:3690 \
+    qt72k1wy/svn
+
+# 进入容器
+docker exec -it svn-server /bin/sh
+
+# 创建仓库
+svnadmin create --fs-type fsfs s-repo
+
+# 配置认证
+# /var/opt/svn/s-repo/conf目录中
+# svnserve.conf 中反注释 password-db = passwd
+# passwd 中添加 sophia = a17
+############################ svn ############################
