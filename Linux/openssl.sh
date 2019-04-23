a2bb7e2a956d84539b5dec1736fbdcac09cee4d2
fzcfydcz <fzcfydcz@163.com>
2019-04-23 22:45:04 +0800

    更新 openssl.sh

--- Linux/openssl.sh
+++ Linux/openssl.sh
@@ -1,6 +1,6 @@
 # Generating SSL/TLS Certificate and Private Key 生成SSL/TLS证书及私钥
 mkdir /etc/ssl/private
-openssl req -x509 -nodes -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem -days 365 -newkey rsa:2048
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem
 #    解释
 #        req - X.509证书签名请求管理的命令
 #        x509 - X.509证书数据管理
@@ -9,3 +9,9 @@ openssl req -x509 -nodes -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/priva
 #        rsa:2048 - RSA key处理器
 #        keyout - key文件
 #        out - 证书文件，注意证书与key在同一个文件中
+
+# 生成私钥及证书
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt
+
+# 使用私钥及证书生成pkcs #12
+openssl pkcs12 -export -out certificate.pfx -inkey privateKey.key -in certificate.crt
