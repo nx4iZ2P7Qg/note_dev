d23a97537228e109f3797c7da2499e6ecaf377e8
fzcfydcz <fzcfydcz@163.com>
2020-03-08 18:22:46 +0800

    update Linux/docker_demo.sh.

--- Linux/docker_demo.sh
+++ Linux/docker_demo.sh
@@ -1,3 +1,20 @@
+# 安装docker
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce
+systemctl start docker
+docker --version
+
+安装docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
 #!/usr/bin/env bash
 
 sudo usermod -aG docker <username>
