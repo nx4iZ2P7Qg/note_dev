e1ab89fa7e139fb388dc608b7ded42a39c3e946c
fzcfydcz <fzcfydcz@163.com>
2020-07-02 16:23:15 +0800

    update Linux/lvm.sh.

--- Linux/lvm.sh
+++ Linux/lvm.sh
@@ -1,3 +1,7 @@
+# pv 对应硬盘分区
+# vg 对应一组 pv
+# lv 对应逻辑分区
+
 # 创建pv
 pvcreate /dev/sdb1
 # 扩展已存在的vg
@@ -6,3 +10,20 @@ vgextend centos /dev/sdb1
 lvextend -L +31G /dev/centos/root
 # 扩展
 xfs_growfs /dev/centos/root
+
+
+# 删除 /home lv，全部扩展至 /
+# 卸载
+umount /home
+# 移除lv
+lvremove /dev/centos/home
+# 查看vg剩余空间
+vgs
+# 扩展相应lv
+lvextend -L +856.88G /dev/centos/root
+# 生效 ext
+resize2fs /dev/centos/root
+# 生效 xfs
+xfs_growfs /dev/centos/root
+# 删除 /home 的自动挂载
+vi /etc/fstab
