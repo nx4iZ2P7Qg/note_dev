c3630f15bf887571a4e49fe24c3801af2efc62a5
nx4iZ2P7Qg <49739484+nx4iZ2P7Qg@users.noreply.github.com>
2020-09-21 10:36:04 +0800

    Update docker.sh

--- Linux/docker.sh
+++ Linux/docker.sh
@@ -1,14 +1,24 @@
-# 安装docker
+# 安装docker centos7
 ----------------------------------------------------------------------------------------------
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install -y docker-ce
 systemctl start docker
+systemctl enable docker
 docker --version
 
-安装docker-compose
+# 安装docker centos8
 ----------------------------------------------------------------------------------------------
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y docker-ce --nobest
+systemctl start docker
+systemctl enable docker
+docker --version
+
+# 安装docker-compose
+----------------------------------------------------------------------------------------------
+# 看一下版本 https://github.com/docker/compose/releases
+curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
